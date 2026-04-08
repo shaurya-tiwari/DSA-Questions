2d2de56fc47adb48175f4a13238fd95f08f54508
shaurya <shauryatiwari120@gmail.com>
2026-04-08 16:12:02 +0530

    new ++

--- ARRAY/MaxConsecutiveOnes.java
+++ ARRAY/MaxConsecutiveOnes.java
@@ -1,25 +1,27 @@
-import java.util.HashSet;
-
 public class MaxConsecutiveOnes {
 
     public int findMaxConsecutiveOnes(int[] nums) {
-        int current_count = 0;
-        HashSet<Integer> maxcount = new HashSet<>();
-
+        int currentcount = 0;
+        int courrentstreak = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                current_count++;
+                currentcount++;
+
+            } else
+                currentcount = 0;
+            if (courrentstreak < currentcount) {
+                courrentstreak = currentcount;
             }
         }
-        
 
-        return 0;
+        return courrentstreak;
     }
 
     public static void main(String[] args) {
         MaxConsecutiveOnes mx = new MaxConsecutiveOnes();
 
         int[] nums = { 1, 1, 0, 1, 1, 1 };
+        int[] nums2 = { 1, 0, 1, 1, 0, 1 };
         System.out.println(mx.findMaxConsecutiveOnes(nums));
     }
 }
