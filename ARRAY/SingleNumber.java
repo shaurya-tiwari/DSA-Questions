b6001d3c24d989b9a431b438eb5894da0212726f
shaurya <shauryatiwari120@gmail.com>
2026-04-10 14:55:44 +0530

    ,,,

--- ARRAY/SingleNumber.java
+++ ARRAY/SingleNumber.java
@@ -1,21 +1,24 @@
+import java.util.HashMap;
+
 public class SingleNumber {
 
     public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> count = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[i] != nums[j]) {
-                    return nums[j];
-                }
-            }
+            count.put(i, count.getOrDefault(count, 1));
         }
-        return 0;
+
+        for (int i : count.keySet()) {
+            
+        }
+        return;
 
     }
 
     public static void main(String[] args) {
         SingleNumber SingleNumber = new SingleNumber();
 
-        int[] arr = {2,2,5,5,7,8,8,9,9 };
-       System.out.println( SingleNumber.singleNumber(arr));
+        int[] arr = { 2, 2, 5, 5, 7, 8, 8, 9, 9 };
+        System.out.println(SingleNumber.singleNumber(arr));
     }
 }
