86fdb9fb9efcf859bf87aa69ab570d5d4c8d563b
shaurya <shauryatiwari120@gmail.com>
2026-04-12 00:16:23 +0530

    new

--- ARRAY/SingleNumber.java
+++ ARRAY/SingleNumber.java
@@ -1,24 +1,30 @@
-import java.util.HashMap;
 
+//  we cant use hashmap because it need to be in o(1)
 public class SingleNumber {
-
+    // USE XOR for check 2 numbers are return 0 .
+    // when two are same theen last one is cancel 
     public int singleNumber(int[] nums) {
-        HashMap<Integer, Integer> count = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            count.put(i, count.getOrDefault(count, 1));
-        }
 
-        for (int i : count.keySet()) {
-            
+        int number = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            number = number ^ nums[i];
         }
-        return;
+        return number;
 
     }
 
     public static void main(String[] args) {
         SingleNumber SingleNumber = new SingleNumber();
 
-        int[] arr = { 2, 2, 5, 5, 7, 8, 8, 9, 9 };
+        int[] arr = { 2,2,1 };
         System.out.println(SingleNumber.singleNumber(arr));
     }
 }
+
+
+// Iska matlab:
+
+// Pehla 2 aaya -> value 2 bani
+// Dusra 2 aaya -> pehle wale 2 ko cancel kar diya
+// Sirf 1 bacha
