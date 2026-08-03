a9a777c4d85f9087052b46ffff8ba04d6e0280fc
shaurya-tiwari <shauryatiwari120@gmail.com>
2026-08-03 22:57:52 +0530

    new

---
+++ ARRAY/recurrealitn.java
@@ -0,0 +1,17 @@
+package ARRAY;
+
+public class recurrealitn {
+        public double myPow(double x, int n) {
+        if (x<0) {
+            x=1/x;
+            n = -n;
+        }
+
+        int ans = ;
+
+
+
+
+            return 0.1;
+    }
+}
