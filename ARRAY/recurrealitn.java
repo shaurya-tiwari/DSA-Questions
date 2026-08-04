35f2ba158553135e7ecb8b011cb5292eed7b7c33
shaurya-tiwari <shauryatiwari120@gmail.com>
2026-08-05 00:25:54 +0530

    ..

--- ARRAY/recurrealitn.java
+++ ARRAY/recurrealitn.java
@@ -1,17 +1,25 @@
 package ARRAY;
 
 public class recurrealitn {
-        public double myPow(double x, int n) {
-        if (x<0) {
-            x=1/x;
-            n = -n;
+    public int search(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+
+            else {
+                right = mid - 1;
+            }
         }
 
-        int ans = ;
+        return -1;
 
-
-
-
-            return 0.1;
     }
 }
