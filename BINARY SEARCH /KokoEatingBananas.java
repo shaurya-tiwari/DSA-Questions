d0d40c972dc110cc58aaaa707e45e7006c39dd95
shaurya-tiwari <shauryatiwari120@gmail.com>
2026-09-19 23:00:30 +0530

    nn

--- BINARY SEARCH /KokoEatingBananas.java
+++ BINARY SEARCH /KokoEatingBananas.java
@@ -1,14 +1,44 @@
 public class KokoEatingBananas {
-    
+//  TC = n log m 
+
     public int minEatingSpeed(int[] piles, int h) {
-        
+        int left = 1;
+        int right = 0;
+        for (int i : piles) {
 
+            if (right < i) {
+                right = i;
+            }
+        }
+        int answer = -1;
 
-        return 0 ;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int totaltimeofkoko = helperfunctionfortimecalculate(piles, mid);
+            if (totaltimeofkoko <= h) {
+                answer = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+
+        }
+
+        return answer;
     }
 
+    // hel[er cuntin ]
+    public static int helperfunctionfortimecalculate(int[] piles, int k) {
+
+        int totaltime = 0;
+        for (int i : piles) {
+            totaltime += Math.ceil((double) i / k); // add time with prevous time with uppr value
+        }
+        return totaltime;
+
+    }
 
     public static void main(String[] args) {
-        
+
     }
 }
---
+++ BINARY SEARCH /MinimumNumberofDaystoMakemBouquets.java
@@ -0,0 +1,20 @@
+
+
+public class MinimumNumberofDaystoMakemBouquets {
+    public int minDays(int[] bloomDay, int m, int k) {
+        int left = 0 ;
+        int right = bloomDay.length-1;
+        
+        while (left<right) {
+            
+        }
+
+
+        return 0;
+    }
+    
+
+    public static void main(String[] args) {
+        
+    }
+}
